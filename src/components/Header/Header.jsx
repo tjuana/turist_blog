8e6f63b0ec214e6b0a9bdd980ee06de01911ae23
Vilena <VAleGuslyakova@sberbank.ru>
2021-07-10 22:50:10 +0300

    Градиентик и новый вид карточек

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeaderStyle } from './header.style'
+
+export const Header = () => {
+    return (
+        <HeaderStyle>
+            ТУТ ТИПА БУДЕТ ХЕДЕР
+        </HeaderStyle>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { CenterStyled } from '../index.style';
 import { Card } from './Card/Card';
+import { GradientCard } from './Card/GradientCard';
+import { Header } from './Header/Header';
+import { GradientStyle } from './main.style';
 
 export const Main = () => {
 
     return(
         <CenterStyled>
             {/* Redux */}
-            <Card icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
-            <Card icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
-            <Card icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
-            <Card icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
-            <Card icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
-            <Card icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
+            <GradientStyle>
+                <Header />
+                {/* Из массива должно браться первых три элемента */}
+                <GradientCard icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
+                <GradientCard icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
+                <GradientCard icon='img/example.png' title="Как мы потерпели фиаско при подборе лодки на Эквадоре" />
+            </GradientStyle>
+            <Card icon='img/example.png' title="1" />
+            <Card icon='img/example.png' title="2" />
+            <Card icon='img/example.png' title="3" />
+            <Card icon='img/example.png' title="4" />
+            <Card icon='img/example.png' title="5" />
+            <Card icon='img/example.png' title="6" />
         </CenterStyled>
     )
 }
