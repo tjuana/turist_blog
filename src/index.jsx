98f99c5b9c47a1d896f276ea88d71fcacbecf15c
Vilena <VAleGuslyakova@sberbank.ru>
2021-07-10 21:15:08 +0300

    Теперь много и правильно

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,20 @@ import 'regenerator-runtime/runtime'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from '@emotion/react'
-import { Provider } from 'react-redux'
 
-import { store } from './__data__'
 import THEME from './styles/theme-colors'
 import { Main } from './components/Main'
+import { CenterStyled, MediaStyled } from './index.style'
 
 const Application = () => {
     return (
         <ThemeProvider theme={THEME.MORNING}>
-            <Main />
+            <CenterStyled>
+                <MediaStyled>
+                    {/* Тут страницы */}
+                    <Main />
+                </MediaStyled>
+            </CenterStyled>
         </ThemeProvider>
     )
 }
